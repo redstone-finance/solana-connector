b9c97f6d269e8cfc651f9c2ccb4a0876613724a0
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-09-28 01:43:50 +0200

    fix up deps

--- examples/src/config.ts
+++ examples/src/config.ts
@@ -1,4 +1,4 @@
-import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import bs58 from "bs58";
 import { Connection, Keypair } from "@solana/web3.js";
 import { Command } from "commander";
 import { readFile } from "fs/promises";
